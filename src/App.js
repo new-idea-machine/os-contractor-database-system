58d1f1f058639c7ea5b77358c856061a95e14c28
SkapeBreak <oriskatebreakginal@gmail.com>
2023-03-03 10:43:12 -0700

    firebase project init

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import 'react-toastify/dist/ReactToastify.css';
 import './styles/app.css';
 import { Home, NotFound, AddContractor, ContractorList } from './pages';
 import ContractorContext from './contexts/ContractorContext';
+import AuthControl from './services/auth';
 
 function App() {
 	return (
 		<>
 			<div className='App'>
-				<ContractorContext>
-					<Router>
-						<Routes>
-							<Route path='*' element={<NotFound />} />
-							<Route path='/' element={<Home />} />
-							<Route path='/contractorList' element={<ContractorList />} />
-							<Route path='/addContractor' element={<AddContractor />} />
-						</Routes>
-					</Router>
-				</ContractorContext>
+				<AuthControl>
+					<ContractorContext>
+						<Router>
+							<Routes>
+								<Route path='*' element={<NotFound />} />
+								<Route path='/' element={<Home />} />
+								<Route path='/contractorList' element={<ContractorList />} />
+								<Route path='/addContractor' element={<AddContractor />} />
+							</Routes>
+						</Router>
+					</ContractorContext>
+				</AuthControl>
 				<ToastContainer />
 			</div>
 		</>
