611d995d25f38a58ad460f1e63aa845e89219825
SkapeBreak <oriskatebreakginal@gmail.com>
2023-03-03 17:15:53 -0700

    updateForm works with initial schema fields

--- src/contexts/ContractorContext.jsx
+++ src/contexts/ContractorContext.jsx
@@ -1,20 +1,58 @@
 import React, { useState, useEffect, createContext } from 'react';
 // import axios from 'axios';
 import data from '../constants/data';
+import { store, auth, db, fbFunctions } from '../firebaseconfig';
+import {
+	doc,
+	addDoc,
+	//   getDoc,
+	//   onSnapshot,
+	setDoc,
+	serverTimestamp,
+	updateDoc,
+	collection,
+	//   query,
+	getDocs,
+	//   where,
+	//   orderBy,
+} from 'firebase/firestore';
 
 export const contractorContext = createContext();
 
 const ContractorContext = ({ children }) => {
-	const [contractorList, setContractorList] = useState(data?.contractors);
+	const [contractorList, setContractorList] = useState([]);
 	const [contractor, setContractor] = useState(null);
 
-	// useEffect(() => {}, [setContractorList(data?.contractors)]);
+	const getCollection = async () => {
+		const querySnapshot = await getDocs(collection(db, 'techs'));
+		const documents = querySnapshot.docs.map((doc) => ({
+			id: doc.id,
+			...doc.data(),
+		}));
+		// console.log('first', documents);
+		setContractorList(documents);
+	};
+	useEffect(() => {
+		getCollection();
+	}, []);
+
+	const updateTechObject = async (data) => {
+		console.log('TRYING TO UPDATE');
+		const userDocRef = doc(db, 'techs', data?.id);
+		if (userDocRef) {
+			await updateDoc(userDocRef, data);
+			console.log('User successfully updated!');
+		} else {
+			console.log('object not found');
+		}
+	};
 
 	const appStates = {
 		contractorList,
 		setContractorList,
 		setContractor,
 		contractor,
+		updateTechObject,
 	};
 
 	return (
--- src/pages/UpdateProfile.jsx
+++ src/pages/UpdateProfile.jsx
@@ -7,7 +7,6 @@ export default function UpdateProfile() {
 		<>
 			<Navigation />
 			<ProfileForm />
-
 			<Footer />
 		</>
 	);
