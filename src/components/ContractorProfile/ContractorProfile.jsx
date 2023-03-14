ddd3f39e03b4031548aed6a19b261540cdae5072
Sanja <98988245+Sisi000@users.noreply.github.com>
2023-03-14 11:15:14 -0600

    Added icons for links on contractors profile

--- src/components/ContractorProfile/ContractorProfile.jsx
+++ src/components/ContractorProfile/ContractorProfile.jsx
@@ -4,6 +4,10 @@ import { useParams } from "react-router-dom";
 import { Footer, Navigation } from "../index";
 import { contractorContext } from "../../contexts/ContractorContext";
 import { skillsContext } from "../../contexts/SkillsContext";
+import { Button } from "@mui/material";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const ContractorProfile = (props) => {
   const { id } = useParams();
@@ -36,77 +40,90 @@ const ContractorProfile = (props) => {
             </div>
             <div className="contractor_info">
               <div className="contractor_name">{contractor?.name}</div>
-              <a
-                style={{ marginBottom: "10px" }}
-                href={`mailto:${contractor?.email}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {contractor?.email}
-              </a>
-              {contractor?.otherInfo?.githubUrl && (
-                <a
-                  href={contractor?.otherInfo?.githubUrl}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  GitHub
-                </a>
-              )}
-              {contractor?.otherInfo?.linkedinUrl && (
+              <div className="contractor_links">
                 <a
                   style={{ marginBottom: "10px" }}
-                  href={contractor?.otherInfo?.linkedinUrl}
+                  href={`mailto:${contractor?.email}`}
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  LinkedIn
+                  <MailOutlineIcon />
                 </a>
-              )}
+                {contractor?.otherInfo?.githubUrl && (
+                  <a
+                    href={contractor?.otherInfo?.githubUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <GitHubIcon />
+                  </a>
+                )}
+                {contractor?.otherInfo?.linkedinUrl && (
+                  <a
+                    style={{ marginBottom: "10px" }}
+                    href={contractor?.otherInfo?.linkedinUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <LinkedInIcon />
+                  </a>
+                )}
+                {contractor?.resume && (
+                  <a
+                    style={{
+                      cursor: "pointer",
+                      margin: "0",
+                      border: "0",
+                      padding: "0",
+                      width: "200px",
+                    }}
+                    href={contractor?.resume}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Download Resume
+                  </a>
+                )}
+              </div>
               {contractor?.projects && (
                 <div>
-                  Projects:{" "}
+                  Projects{" "}
                   {contractor?.projects.map((project) => (
                     <div
                       key={project.id}
                       style={{
                         backgroundColor: "#D5D1D0",
                         marginTop: "5px",
-                        marginBottom: "5px",
+                        marginBottom: "15px",
                         borderRadius: "5px",
                         padding: "5px",
                       }}
                     >
-                      <div>Project Name: {project?.projectName}</div>
-                      <div>Project Description: {project?.description}</div>
+                      <div><b>Project Name:</b><br/> {project?.projectName}</div>
+                      <div><b>Project Description:</b><br/> {project?.description}</div>
                     </div>
                   ))}
                 </div>
               )}
-                {contractor?.skillIds && (
-              <div>
-                Skills:
-                {contractorSkills.map((resultSkill) => {
-                  console.log(resultSkill);
-                  return <div key={resultSkill.id}>{resultSkill.title}</div>;
-                })}
-              </div>
-                )}
-              {contractor?.resume && (
-                <a
-                  style={{
-                    cursor: "pointer",
-                    margin: "0",
-                    border: "0",
-                    padding: "0",
-                    width: "200px",
-                  }}
-                  href={contractor?.resume}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Download Resume
-                </a>
+              {contractor?.skillIds && (
+                <div>
+                  {contractorSkills.map((resultSkill) => {
+                    return (
+                      <Button
+                        key={resultSkill.id}
+                        style={{
+                          borderStyle: "solid",
+                          borderWidth: "1px",
+                          padding: "2px",
+                          marginLeft: "5px",
+                          textTransform: "capitalize",
+                        }}
+                      >
+                        {resultSkill.title}
+                      </Button>
+                    );
+                  })}
+                </div>
               )}
             </div>
           </div>
