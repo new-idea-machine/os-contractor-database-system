d6e20567f4837d0aa74b2f549c1fbf739f9fdec2
GitCollab2022 <tatiquintana@hotmail.com>
2023-04-26 10:57:30 -0600

    SkillsData file created

---
+++ src/contexts/skillsData.js
@@ -0,0 +1,10 @@
+
+const developerSkills = ["Firebase", "ExpressJS", "MongoDB", "NodeJS", "ReactJS", "Back-end", "Front-end", "Linux", "Mac", "Windows", "Docker", "Kubernets", "MySQL", "Oracle", "MS SQL Server", "PostgreSQL", "Redis", "SQL", "Agile", "Angular", "Bootstrap", "C", "C#", "C++", "Cassandra", "CSS3", "Flutter", "Git", "Go", "HTML5", "Ionic", "Java", "JavaScript", "PHP", "Python", "React Native", "Ruby", "Scala", "TypeSript", "Xamarin"];
+
+const designerSkills = ["Front-end", "Database", "Data Visualization", "Figma", "Sketch", "Photoshop", "Adobe Illustrator", "JavaScript", "CSS3", "HTML5", "Webflow", "Adobe XD", "Python", "Swift", "C++", "UI", "UX", "Agile", "Game", "Web", "Wireframing", "Redesign", "Responsive", "Software", "Mockups", "Mobile", "Application", "WordPress", "WooCommerce", "Joomla", "Drupal", "Shopify"];
+
+const productManagerSkills = ["Agile", "App", "Data", "Digital Marketing", "eCommerce", "Jira", "Kanban", "Lean", "Game", "Web", "Mobile", "Software", "Scrum", "UX"];
+
+const projectManagerSkills = ["Agile", "Asana", "Atlassian", "Jira", "Kanban", "Lean", "Mobile", "Game", "Software", "Web", "Monday", "PMI", "PMO", "PMP", "Product Delivery", "Scrum", "Waterfall", "eCommerce"];
+
+export {developerSkills, designerSkills, productManagerSkills, projectManagerSkills}
