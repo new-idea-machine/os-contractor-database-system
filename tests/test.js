f8741d967faf47f945c05a61794f737c66c0edae
Afshin Sharifnia <afshin.sharifnia@gmail.com>
2023-05-16 12:04:17 -0600

    TC01-modified

--- tests/test.js
+++ tests/test.js
@@ -5,10 +5,17 @@ async function test_case1() {
 
   await driver.get("http://localhost:3000");
   await driver.findElement(By.className("customButton4")).click();
+
   await driver
     .findElement(By.id("emailInput"))
     .sendKeys("qa@qa.com", Key.RETURN);
-  await driver.findElement(By.name("password")).sendKeys("123456", Key.RETURN);
+  await driver
+    .findElement(By.xpath("/html/body/div/div[2]/div/div[2]/div[1]/input[2]"))
+    .sendKeys("123456", Key.RETURN);
+
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div[2]/div/div[2]/div[1]/button'))
+    .click();
   setInterval(function () {
     driver.quit();
   }, 20000);
