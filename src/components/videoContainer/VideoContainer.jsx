40a3e09870306f2991bf62b37a90a43ac34d8f46
Kevin Woodman <kwoodman1970@yahoo.ca>
2024-02-09 00:24:54 -0700

    Simplified VideoContainer.jsx component

--- src/components/videoContainer/VideoContainer.jsx
+++ src/components/videoContainer/VideoContainer.jsx
@@ -4,22 +4,16 @@ import video from '../../assets/work-space.mp4';
 
 import './videoContainer.css';
 
-const VideoContainer = () => {
-	const vidRef = React.useRef();
-
+function VideoContainer() {
 	return (
-		<div className='app__video flex__center'>
-			<video
-				className='app__video-position'
-				ref={vidRef}
-				src={video}
-				type='video/mp4'
-				loop
-				controls={false}
-				muted
-				autoPlay
-			/>
-		</div>
+		<video
+			src={video}
+			type='video/mp4'
+			loop
+			controls={false}
+			muted
+			autoPlay
+		/>
 	);
 };
 
