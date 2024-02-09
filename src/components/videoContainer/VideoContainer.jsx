ddfb0854d76d7113b1fee1af39f5aa39e6d25425
Kevin Woodman <kwoodman1970@yahoo.ca>
2024-02-08 22:34:03 -0700

    Changed Home.jsx page to more closely match new UX/UI design

--- src/components/videoContainer/VideoContainer.jsx
+++ src/components/videoContainer/VideoContainer.jsx
@@ -1,63 +1,24 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React from 'react';
+
 import video from '../../assets/work-space.mp4';
-// import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
-// import { meal } from '../../constants';
-import { useNavigate } from 'react-router-dom';
+
 import './videoContainer.css';
-import { authContext } from '../../contexts/auth';
-// import ActionBar from '../../components/actionBar/ActionBar';
 
 const VideoContainer = () => {
-	const { user, logout } = useContext(authContext);
-	// const [playVideo, setPlayVideo] = useState(false);
-	const navigate = useNavigate();
 	const vidRef = React.useRef();
-
-	// const handleVideo = () => {
-	// 	setPlayVideo((prevPlayVideo) => !prevPlayVideo);
-	// 	if (playVideo) {
-	// 		vidRef.current.pause();
-	// 	} else {
-	// 		vidRef.current.play();
-	// 	}
-	// };
 
 	return (
 		<div className='app__video flex__center'>
-			<div>
-				<video
-					className='app__video-position'
-					ref={vidRef}
-					src={video}
-					type='video/mp4'
-					loop
-					controls={false}
-					muted
-					autoPlay
-				/>
-			</div>
-			<div className='app__video-overlay flexCenter'>
-				{/* <div className=' ' onClick={handleVideo}> */}
-				{/* <h1 className='headtext__cormorant'>Welcome</h1> */}
-				{/* <ActionBar /> */}
-				<h1 style={{ textAlign: 'center', color: 'white' }}>
-					Welcome to our contractor database system, which is a software
-					application that enables users to store, manage, and access
-					information related to contractors.
-				</h1>
-				<div className='customButton4' onClick={() => navigate('auth')}>
-					<span>Log In Or Sign Up</span>
-				</div>
-				{user && (
-					<div className='customButton4' onClick={() => logout()}>
-						<span>Logout</span>
-					</div>
-				)}
-				{/* {playVideo ? (
-          <BsPauseFill color='#fff' fontSize={30}/>
-        ) : <BsFillPlayFill color='#fff' fontSize={30}/>} */}
-				{/* </div> */}
-			</div>
+			<video
+				className='app__video-position'
+				ref={vidRef}
+				src={video}
+				type='video/mp4'
+				loop
+				controls={false}
+				muted
+				autoPlay
+			/>
 		</div>
 	);
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,39 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+import { authContext } from '../contexts/auth';
 import VideoContainer from '../components/videoContainer/VideoContainer';
 
+import './Home.css';
+
 export default function Home() {
+	const { user, logout } = useContext(authContext);
 	const navigate = useNavigate();
+
 	return (
 		<>
 			<VideoContainer />
-			{/* <div className='homePage flexCenter'>
-				<h1 style={{ textAlign: 'center', color: 'white' }}>
-					Welcome to our contractor database system, which is a software
-					application that enables users to store, manage, and access
-					information related to contractors.
-				</h1>
-				<div
-					className='customButton4'
-					onClick={() => navigate('contractorlist')}
-				>
-					<span>Continue</span>
-				</div>
-			</div> */}
+
+			<main>
+				<header id="top_left"><h2>CONTRACTOR DB</h2></header>
+				<section id="middle">
+					<h1>Welcome to our contractor database system,</h1>
+					<p>
+						a software application that enables users to store, manage, and
+						access information related to contractors.
+					</p>
+					{user ?
+						<div className='customButton4' onClick={() => navigate('auth')}>
+							<span>Log In Or Sign Up</span>
+						</div> :
+						<div className='customButton4' onClick={() => logout()}>
+							<span>Logout</span>
+						</div>
+					}
+				</section>
+				<footer id="bottom_left"><h3>CONTRACTOR DB</h3></footer>
+				<footer id="bottom_right">&copy; New Idea Machine 2023. All rights reserved.</footer>
+			</main>
 		</>
 	);
 }
