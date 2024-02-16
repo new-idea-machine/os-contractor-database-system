0f1462e6217c60ac2b20c537f17961db502e26da
Kevin Woodman <kwoodman1970@yahoo.ca>
2024-02-15 23:43:54 -0700

    Began changing the contents of Navigation.jsx components to match new UX/UI style

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -18,6 +18,8 @@ import FavoriteBorderRoundedIcon from "@mui/icons-material/FavoriteBorderRounded
 import BackgroundLetterAvatars from "./BackgroundLetterAvatars";
 import MailOutlineRoundedIcon from '@mui/icons-material/MailOutlineRounded';
 
+import avatar from "../../assets/avatar.png";
+
 function getURLPath(URL) {
   /*
   This function returns the part portion of "URL" (which MUST be a well-formed uniform resource
@@ -67,11 +69,41 @@ const Navigation = () => {
 
   return (
     <nav>
+      <h2>CONTRACTOR <b>DB</b></h2>
+
+      <div className="profilePicture">
+        {/* {currentUserProfile?.profileImg ?
+          <img src={currentUserProfile?.profileImg} alt="Profile" /> :
+          <img src={avatar} alt="Profile" />
+        } */}
+        <img src={currentUserProfile.profileImg ? currentUserProfile.profileImg : avatar} alt="Profile" />
+      </div>
+
+
+      {/* <Avatar sx={{ width: 45, height: 45 }}>
+        {currentUserProfile?.profileImg ?
+          <div className="image_container">
+            <img src={currentUserProfile.profileImg} alt="" />
+          </div> :
+          <BackgroundLetterAvatars
+            currentUserFirstName={currentUserProfile?.firstName}
+            currentUserLastName={currentUserProfile?.lastName}
+          />
+          // (recruiterMap && recruiterMap[user?.uid] ?
+          //   <BackgroundLetterAvatars
+          //     currentUserFirstName={recruiterMap[user?.uid]?.firstName} // Update the props
+          //     currentUserLastName={recruiterMap[user?.uid]?.lastName} // Update the props
+          //   /> :
+          //   null)
+        }
+      </Avatar> */}
+      {/* <MenuItem onClick={() => navigate("/updateProfile")}> */}
+      {/* <MenuItem onClick={() => navigate("/myProfile")} sx={{width: "160px"}}> */}
+
       <ul>
         <li>
           <NavLink
-            className="navbar-links"
-            style={{ color:  "var(--navigation-colour-distinct)" }}
+            active={currentPage === "contractorlist"}
             activeclassname="selected"
             exact="true"
             to="/contractorList"
@@ -82,7 +114,7 @@ const Navigation = () => {
 
         <li>
           <NavLink
-            className="navbar-links"
+            active={currentPage === "search"}
             activeclassname="selected"
             exact="true"
             to="/search"
@@ -94,148 +126,25 @@ const Navigation = () => {
 
         <li>
           <NavLink
-            className="navbar-links"
+            active={currentPage === "About"}
             activeclassname="selected"
             exact="true"
             to="/About"
-
           >
             About
           </NavLink>
         </li>
-        {user ? (
-          <>
-            <Tooltip>
-              <IconButton
-                onClick={handleClick}
-                size="small"
-                sx={{ ml: 2 }}
-                aria-controls={open ? "account-menu" : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? "true" : undefined}
-              >
-                <Avatar sx={{ width: 45, height: 45 }}>
-                  {" "}
-                  {currentUserProfile ? (
-                    <>
-                      {currentUserProfile?.profileImg ? (
-                        <>
-                          <div className="image_container">
-                            <img src={currentUserProfile?.profileImg} alt="" />
-                          </div>
-                        </>
-                      ) : (
-                        <BackgroundLetterAvatars
-                          currentUserFirstName={currentUserProfile?.firstName}
-                          currentUserLastName={currentUserProfile?.lastName}
-                        />
-                      )}
-                    </>
-                    ) : recruiterMap && recruiterMap[user?.uid] ? (
-                    <BackgroundLetterAvatars
-                      currentUserFirstName={recruiterMap[user?.uid]?.firstName} // Update the props
-                      currentUserLastName={recruiterMap[user?.uid]?.lastName} // Update the props
-                    />
-                  ) : null}
-                </Avatar>
-              </IconButton>
-            </Tooltip>
-            <Menu
-              anchorEl={anchorEl}
-              id="account-menu"
-              open={open}
-              onClose={handleClose}
-              onClick={handleClose}
-              PaperProps={{
-                elevation: 0,
-                sx: {
-                  overflow: "visible",
-                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                  mt: 1.5,
-                  "& .MuiAvatar-root": {
-                    width: 32,
-                    height: 32,
-                    ml: -0.5,
-                    mr: 1,
-                  },
-                  "&:before": {
-                    content: '""',
-                    display: "block",
-                    position: "absolute",
-                    top: 0,
-                    right: 14,
-                    width: 10,
-                    height: 10,
-                    bgcolor: "background.paper",
-                    transform: "translateY(-50%) rotate(45deg)",
-                    zIndex: 0,
-                  },
-                },
-              }}
-              transformOrigin={{ horizontal: "right", vertical: "top" }}
-              anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-            >
-              <MenuItem onClick={() => navigate("/myProfile")} sx={{width: "160px"}}>
-                <Avatar
-                  sx={{
-                    width: "25px!important",
-                    height: "25px!important",
-                    marginLeft: "1px!important",
-                  }}
-                />{" "}
-                My Profile
-              </MenuItem>
-              <MenuItem onClick={() => navigate("/updateProfile")}>
-                <Avatar
-                  sx={{
-                    width: "25px!important",
-                    height: "25px!important",
-                    marginLeft: "1px!important",
-                  }}
-                />{" "}
-                Edit My Profile
-              </MenuItem>
-              <MenuItem onClick={() => navigate("/inbox")}>
-                <MailOutlineRoundedIcon
-                  sx={{ width: 25, height: 25, color: "gray", marginRight: 1 }}
-                />{" "}
-                Messages({unreadMessages})
-              </MenuItem>
-              <MenuItem onClick={() => navigate("/favorites")}>
-                <FavoriteBorderRoundedIcon
-                  sx={{ width: 25, height: 25, color: "gray", marginRight: 1 }}
-                />{" "}
-                Favorites
-              </MenuItem>
-              <Divider />
-              <MenuItem
-                onClick={() => {
-                  logout();
-                  navigate("/contractorList");
-                }}
-              >
-                <ListItemIcon>
-                  <Logout
-                    fontSize="small"
-                    sx={{ color: "gray", marginLeft: 0.5 }}
-                  />
-                </ListItemIcon>
-                Logout
-              </MenuItem>
-            </Menu>
-          </>
-        ) : (
-          <li style={{ marginRight: "30px" }}>
-            <NavLink
-              className="navbar-links"
-              activeclassname="selected"
-              exact
-              to="/auth"
-            >
-              Log in
-            </NavLink>
-          </li>
-        )}
+
+        <li>
+          <NavLink
+            active={currentPage.substring(0, 5) === "chat/"}
+            activeclassname="selected"
+            exact="true"
+            to="/About"
+          >
+            Chat
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
