71c066bb1d38e858ad725ac3daba3d088c748648
Kevin Woodman <kwoodman1970@yahoo.ca>
2024-02-29 15:54:56 -0700

    Made About.jsx page comply more closely with new UX/UI style

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,18 +5,20 @@ import { Navigation } from '../components';
 import ProfilePicture from '../components/ProfilePicture';
 import Networking from ".//../assets/networking.jpg";
 
+import './About.css';
+
 export default function About() {
   return (
     <>
       <Navigation />
-      <main className='aboutPage'>
-        <div className='imgContainer'>
-          <h2 className='creators'> Meet our Team: </h2>
+      <main>
+        <h1>Meet Our Team</h1>
 
-          <h2 className='motto' role='textbox' aria-multiline='true'>
-          This application is designed to help Recruiters to find a perfect fit for their company&apos;s next project and to help Contractors utilize their talent.
-          </h2>
-        </div>
+        <p>
+          This application is designed to help Recruiters to find a perfect fit
+          for their company&apos;s next project and to help Contractors utilize
+          their talent.
+        </p>
 
         <div className='teamContainer'>
           <div className='teamCard flexCenter'>
@@ -46,14 +48,7 @@ export default function About() {
               incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
               exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </div>
-            <div className='imageWrapper'>
-              <div className='biography'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              </div>
-              <ProfilePicture profileImage={null} size="200px" />
-            </div>
+            <ProfilePicture profileImage={null} size="200px" />
             <h2 className='teamMember'>Diego Gomez</h2>
             <h3 className='role'>Developer</h3>
           </div>
@@ -73,14 +68,7 @@ export default function About() {
               incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
               exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </div>
-            <div className='imageWrapper'>
-              <div className='biography'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              </div>
-              <ProfilePicture profileImage={null} size="200px" />
-            </div>
+            <ProfilePicture profileImage={null} size="200px" />
             <h2 className='teamMember'>Sanja Ivansic</h2>
             <h3 className='role'>Developer</h3>
           </div>
