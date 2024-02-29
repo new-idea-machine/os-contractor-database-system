b8b9ad95e23ff10a5d503c84698945416efe871d
Kevin Woodman <kwoodman1970@yahoo.ca>
2024-02-29 16:33:50 -0700

    Simplified About.jsx page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,24 @@
-import React, { useContext } from 'react';
-import ContractorCard from '../components/contractorCard/ContractorCard';
-import { contractorContext } from '../contexts/ContractorContext';
+import React from 'react';
 import { Navigation } from '../components';
 import ProfilePicture from '../components/ProfilePicture';
-import Networking from ".//../assets/networking.jpg";
 
 import './About.css';
+
+const teamMembers = [];
+
+// Placeholder biography -- remove when actual biographies are incorporated:
+
+const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing ' +
+  'elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ' +
+  'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ' +
+  'ut aliquip ex ea commodo consequat.';
+
+teamMembers.push({name:  'Ada Opene', role: 'Project Manager', profileImage:  null, biography: loremIpsum});
+teamMembers.push({name:  'Nonso Afulukwe', role: 'Project Owner', profileImage:  null, biography: loremIpsum});
+teamMembers.push({name:  'Diego Gomez', role: 'Developer', profileImage:  null, biography: loremIpsum});
+teamMembers.push({name:  'Afshin Sharifnia', role: 'Quality Assurance', profileImage:  null, biography: loremIpsum});
+teamMembers.push({name:  'Sanja Ivansic', role: 'Developer', profileImage:  null, biography: loremIpsum});
+teamMembers.push({name:  'Irena Skachedub', role: 'Developer', profileImage:  null, biography: loremIpsum});
 
 export default function About() {
   return (
@@ -20,69 +33,17 @@ export default function About() {
           their talent.
         </p>
 
-        <div className='teamContainer'>
-          <div className='teamCard flexCenter'>
-            <div className='biography'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        <section className='teamContainer'>
+          {teamMembers.map((member, index) =>
+            <div className='teamCard flexCenter' key={index}>
+              <div className='biography'>{member.biography}</div>
+              <ProfilePicture profileImage={member.profileImage} size='200px' />
+              <h2>{member.name}</h2>
+              <h3>{member.role}</h3>
             </div>
-            <ProfilePicture profileImage={null} size="200px" />
-            <h2 className='teamMember'>Ada Opene</h2>
-            <h3 className='role'>Project Manager</h3>
-          </div>
-
-          <div className='teamCard flexCenter'>
-            <div className='biography'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </div>
-            <ProfilePicture profileImage={null} size="200px" />
-            <h2 className='teamMember'>Nonso Afulukwe</h2>
-            <h3 className='role'>Product Owner</h3>
-          </div>
-          <div className='teamCard flexCenter'>
-            <div className='biography'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </div>
-            <ProfilePicture profileImage={null} size="200px" />
-            <h2 className='teamMember'>Diego Gomez</h2>
-            <h3 className='role'>Developer</h3>
-          </div>
-          <div className='teamCard flexCenter'>
-            <div className='biography'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </div>
-            <ProfilePicture profileImage={null} size="200px" />
-            <h2 className='teamMember'>Afshin Sharifnia</h2>
-            <h3 className='role'>Quality Assurance </h3>
-          </div>
-          <div className='teamCard flexCenter'>
-            <div className='biography'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </div>
-            <ProfilePicture profileImage={null} size="200px" />
-            <h2 className='teamMember'>Sanja Ivansic</h2>
-            <h3 className='role'>Developer</h3>
-          </div>
-          <div className='teamCard flexCenter'>
-            <div className='biography'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </div>
-            <ProfilePicture profileImage={null} size="200px" />
-            <h2 className='teamMember'>Irina Skachedub</h2>
-            <h3 className='role'>Developer</h3>
-          </div>
-        </div>
+            )
+          }
+        </section>
       </main>
     </>
   );
