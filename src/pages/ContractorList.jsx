03b71414b65de9ad446c5a2d63e546328cca0ba3
Kevin Woodman <kwoodman1970@yahoo.ca>
2024-05-23 13:12:11 -0600

    Added personalized greeting to ContractorList.jsx header as per the new UX/UI style

--- src/pages/ContractorList.jsx
+++ src/pages/ContractorList.jsx
@@ -1,17 +1,19 @@
 import React, { useContext } from 'react';
+import { authContext } from "../contexts/Authorization";
 import { contractorContext } from '../contexts/ContractorContext';
 import { Navigation } from '../components';
 import ResponsiveGrid from '../components/ResponsiveGrid';
 import ContractorCard from '../components/contractorCard/ContractorCard';
 
 export default function ContractorList() {
+	const { user } = useContext(authContext);
 	const { contractorList } = useContext(contractorContext);
 
 	return (
 		<>
 			<Navigation />
 			<main>
-				<h1>Our Available Contractors</h1>
+				<h1>Hi, {user?.displayName ? user.displayName : "there"}!</h1>
 
 				<p>
 					&quot;Our success is determined by new developers finding career
@@ -19,6 +21,8 @@ export default function ContractorList() {
 					help to establish the right attitude in new devs, reducing your risk
 					of hiring for your own development team.&quot;
 				</p>
+
+				<h2>Our Available Contractors</h2>
 
 				<ResponsiveGrid minColumnWidth="310px" rowGap="10px">
 					{contractorList.map((person, index) => (
