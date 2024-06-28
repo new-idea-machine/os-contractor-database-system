8b3f590416535e1856f3c67154bcfbdd8e0b5792
Kevin Woodman <kwoodman1970@yahoo.ca>
2024-06-28 00:21:34 -0600

    Rebuilt the InputSection.jsx component to simplify it and make it comply with the new UX/UI style

--- src/components/inputSection/InputSection.jsx
+++ src/components/inputSection/InputSection.jsx
@@ -1,67 +1,54 @@
 import React from 'react';
 
-export default function InputSection({ field, onChange, value, onDelete}) {
-	return (
-		<>
-			<label>{field?.label}</label>
-			{field?.type === 'textArea' && field?.name !== 'availabilityDetails'? (
-				<div>
+export default function InputSection({ field, onChange, value, onDelete })
+{
+	if (field?.type === 'select') {
+		return (
+			<label>
+				{ field.label && <>{field.label}<br /></> }
+				<select name={ field?.name } value={ value } onChange={ onChange }>
+					{ field?.options?.map((option) => (
+						<option key={option} value={option}>
+							{ option }
+						</option>
+					)) }
+				</select>
+			</label>
+		);
+	} else if (field?.type === 'textArea') {
+		return (
+			<label>
+				{ field.label && <>{field.label}<br /></> }
 				<textarea
 					name={field?.name}
 					value={value}
 					placeholder={field?.placeholder}
 					onChange={onChange}
-					aria-label={field?.label}
 				/>
-					{field?.name === 'description' && value && (
-            <button type="button" onClick={onDelete}>
-              X
-            </button>
-					)}
-				</div>
-					
-			) : field?.type === 'select' ? (
-				<select
-				  name={field?.name}
-				  value={value}
-				  onChange={onChange}
-				  aria-label={field?.label}
-				>
-				  {field?.options?.map((option, index) => (
-					<option key={index} value={option}>
-					  {option}
-					</option>
-				  ))}
-				</select>
-			  ) : (
-				<div>
+				{ onDelete && (
+					<button type="button" onClick={onDelete}>
+						X
+					</button>
+				) }
+			</label>
+		);
+	} else {
+		return (
+			<label>
+				{ field.label && <>{field.label}<br /></> }
 				<input
 					name={field?.name}
 					value={value}
 					placeholder={field?.placeholder}
 					type={field?.type}
 					onChange={onChange}
-					aria-label={field?.label}
-					required ={field?.name !== 'linkedinUrl' && field?.name !== 'githubUrl' && field?.name !=="skill"} 
 				/>
-				{(field?.name === 'skill' || field?.name === 'description') && value && (
-            <button type="button" onClick={onDelete}>
-              X
-            </button>
-          )}
-				</div>
-			
-			)}
-				{field?.name === 'availability' && value === 'Other' && (
-        		// Render additional text area for 'Other' option
-        		<textarea
-          			name= 'availabilityDetails'
-          			//value={value}
-          			placeholder='Please provide more info'
-          			onChange={onChange}
-          			aria-label= 'Availability details'
-        />
-      )}
-		</>
-	);
+				{ onDelete && (
+					<button type="button" onClick={onDelete}>
+						X
+					</button>
+				) }
+			</label>
+		);
+	}
 }
