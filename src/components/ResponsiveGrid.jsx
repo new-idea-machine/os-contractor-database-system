1a1a0c6a01048d4c48638aa92e7bb1a65edcf127
Kevin Woodman <kwoodman1970@yahoo.ca>
2024-08-01 16:13:17 -0600

    Added columnGap property to ResponsiveGrid.jsx component and content justification to "space-between"

--- src/components/ResponsiveGrid.jsx
+++ src/components/ResponsiveGrid.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-function ResponsiveGrid({minColumnWidth, rowGap, children}) {
+function ResponsiveGrid({minColumnWidth, rowGap, columnGap, children}) {
   const style =
     {
       display:  "grid",
       gridTemplateColumns:  `repeat(auto-fit, minmax(${minColumnWidth}, 1fr))`,
       rowGap,
+      columnGap,
       placeItems:  "center",
-      minHeight:  "fit-content"
+      minHeight:  "fit-content",
+      justifyContent:  "space-between"
     }
 
   return (
