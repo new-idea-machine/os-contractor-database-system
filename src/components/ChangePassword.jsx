2867941a6ff7aaccda981193268bcc35b268764f
Kevin Woodman <kwoodman1970@yahoo.ca>
2024-08-02 22:58:56 -0600

    Added code to the ChangePassword.jsx component for validattion, verification and actually changing the user's password

--- src/components/ChangePassword.jsx
+++ src/components/ChangePassword.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState, useContext } from 'react';
+import { toast } from 'react-toastify';
+import { EmailAuthProvider, reauthenticateWithCredential, updatePassword } from 'firebase/auth';
 import { authContext } from '../contexts/Authorization';
 import InputSection from './inputSection/InputSection';
 
@@ -22,11 +24,51 @@ export default function ChangePassword() {
 		getIdToken();
 	}, [user]);
 
+	async function onSubmit(event) {
+		event.preventDefault();
+
+		const passwordStrength = /^(?=.*[0-9])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,}$/;
+
+		const formElements = event.target.elements;
+
+		const currentPassword = formElements.currentPassword.value;
+		const newPassword = formElements.newPassword.value;
+		const confirmPassword = formElements.confirmPassword.value;
+
+		let fieldsAreValid = true;
+
+		if (newPassword !== confirmPassword) {
+			toast.error('The two new passwords aren\'t the same')
+			fieldsAreValid = false;
+		} else if (!passwordStrength.test(newPassword)) {
+			toast.error('Please enter a password with at least 8 characters, one number, one lowercase, one upper case and one special character');
+			fieldsAreValid = false;
+		}
+
+		const credential = EmailAuthProvider.credential(user.email, currentPassword);
+
+		try {
+			await reauthenticateWithCredential(user, credential)
+		} catch {
+			toast.error('The (current) password that you entered is incorrect')
+			fieldsAreValid = false;
+		}
+
+		if (fieldsAreValid) {
+			try {
+				updatePassword(user, newPassword)
+				toast.info('Password successfully changed!');
+			} catch {
+				toast.error('Failed to change password');
+			}
+		}
+	}
+
 	return ((idToken?.signInProvider === 'password') ?
 		<>
 			<h1>Change Password</h1>
 
-			<form id='ChangePassword'>
+			<form id='ChangePassword' onSubmit={onSubmit}>
 				<section id="Passwords">
 					<InputSection field={ { type:  'password', name:  'currentPassword',  label:  'Current password' } } value='' />
 					<InputSection field={ { type:  'password', name:  'newPassword',  label:  'New password' } } value='' />
