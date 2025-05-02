2a0af2ca33252dd146c7f21b2e0b10d6f5dd450c
Kevin Woodman <kwoodman1970@yahoo.ca>
2025-05-01 22:50:59 -0600

    Added search bar to RecruiterFavouriteList.jsx page

--- src/pages/RecruiterFavoriteList.jsx
+++ src/pages/RecruiterFavoriteList.jsx
@@ -1,16 +1,33 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { auth } from "../firebaseconfig";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { toast } from 'react-toastify';
 import { userProfileContext } from '../contexts/UserProfileContext';
 import { favouritesContext } from '../contexts/FavouritesContext';
 import { Navigation } from '../components';
+import SearchBar from '../components/SearchBar';
 import MatchCard from '../components/MatchCard';
 import ContractorProfile from '../components/ContractorProfile/ContractorProfile';
 
+import { parseKeywords, findKeywordsIn } from '../components/Chat/search';
+
 export default function RecruiterFavoriteList() {
+	const navigate = useNavigate();
+	const [user] = useAuthState(auth);
+	const [keywords, setKeywords] = useState("");
 	const { favourites } = useContext(favouritesContext);
 	const { getUserProfile } = useContext(userProfileContext);
 	const favouriteProfiles = favourites.map((techId) => getUserProfile(techId));
 	const [selectedContractor, setSelectedContractor] = useState(null);
 	const [scrollPosition, setScrollPosition] = useState(0.0);
+
+	useEffect(() => {
+		if (user === null) {
+			toast.error('Please Login To View Favourites');
+			navigate('/');
+		}
+	}, [user]);
 
 	useEffect(() => {
 		/*
@@ -25,6 +42,29 @@ export default function RecruiterFavoriteList() {
 		return () => clearTimeout(timeoutId);
 	}, [selectedContractor])
 
+	function filterContractors() {
+		const keywordExpressions = parseKeywords(keywords);
+
+		if (keywordExpressions.length === 0) {
+			return favouriteProfiles;
+		} else {
+			const newContractorsList = [];
+
+			favouriteProfiles.forEach((profile) => {
+				if (findKeywordsIn(keywordExpressions, profile.firstName) ||
+					findKeywordsIn(keywordExpressions, profile.lastName) ||
+					findKeywordsIn(keywordExpressions, profile.summary)
+				) {
+					newContractorsList.push(profile);
+				}
+			});
+
+			return newContractorsList;
+		}
+	}
+
+	const filteredContractors = filterContractors();
+
 	return (
 		<>
 			<Navigation menu="Profile" />
@@ -34,7 +74,8 @@ export default function RecruiterFavoriteList() {
 				{selectedContractor ?
 					<ContractorProfile data={selectedContractor} onClose={() => setSelectedContractor(null)} /> :
 					<>
-						{favouriteProfiles?.map((person) => (
+					        <SearchBar placeholder="Find favourite contractor" defaultValue={ keywords } setSearchTerms={ setKeywords }/>
+						{filteredContractors?.map((person) => (
 							<div key={person.id}>
 								<MatchCard contractor={person} onClick={() => {
 									setScrollPosition(window.scrollY);
