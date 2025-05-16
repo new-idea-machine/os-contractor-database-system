3144ff511ac42f547833adc01d3084830dfb67be
DinhAnh2301 <dinhanh2301@gmail.com>
2025-05-16 06:37:21 -0600

    feat: adding contractors' layout

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,31 +1,34 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import CSCSelector from "./CSCSelector/CSCSelector";
 import { userProfileContext } from "../../contexts/UserProfileContext";
 import Navigation from "../navigation/Navigation";
-import React, {
-  useContext,
-  useState,
-  useRef,
-} from "react";
+import { useContext, useState, useRef, useEffect } from "react";
 import SearchSkills from "./SearchSkills/SearchSkills";
-import { useNavigate } from "react-router-dom";
 import style from "./Search.module.css";
 import { qualificationsList, workSiteList } from "../../constants/data";
+import ContractorProfile from "../ContractorProfile/ContractorProfile";
 
 const avatarURL = "/assets/avatar.svg";
 const favContractor = "/assets/heart-icon.svg";
 
 export default function Search() {
-  const navigate = useNavigate();
   const contractorList = useContext(userProfileContext).contractors;
   const [contractors, setContractors] = useState([]);
   const [country, setCountry] = useState("");
   const [city, setCity] = useState("");
-  const [state, setState] = React.useState("");
+  const [state, setState] = useState("");
   const [selectedSkills, setSelectedSkills] = useState([]);
   const [selectedQualification, setSelectedQualification] = useState([]);
   const [selectedWorkSite, setSelectedWorkSite] = useState([]);
   const contractorListRef = useRef(null);
+  const [selectedContractor, setSelectedContractor] = useState(null);
+  const [scrollPosition, setScrollPosition] = useState(0.0);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      window.scrollTo(0.0, selectedContractor ? 0.0 : scrollPosition);
+    }, 10);
+    return () => clearTimeout(timeoutId);
+  }, [selectedContractor, scrollPosition]);
 
   const filterContractors = () => {
     const calculatePercentMatching = (contractor) => {
@@ -127,148 +130,159 @@ export default function Search() {
       <Navigation />
       <main>
         <h1>Search</h1>
-        <div className={style["search_container"]}>
-          <div>
-            <p className={style["search_description"]}>
-              &quot;Our success is determined by new developers finding career
-              positions in other companies. We foster skill, determine will, and
-              help to establish the right attitude in new devs, reducing your
-              risk of hiring for your own development team.&quot;
-            </p>
+        {!selectedContractor && (
+          <div className={style["search_container"]}>
+            <div>
+              <p className={style["search_description"]}>
+                &quot;Our success is determined by new developers finding career
+                positions in other companies. We foster skill, determine will,
+                and help to establish the right attitude in new devs, reducing
+                your risk of hiring for your own development team.&quot;
+              </p>
+              <div className={style["search_qualification_container"]}>
+                <p>Search by qualifications</p>
+                <div>
+                  {qualificationsList.map((option) => (
+                    <label key={option}>
+                      <input
+                        type="checkbox"
+                        checked={selectedQualification.includes(option)}
+                        onChange={() => handleOptionQualificationChange(option)}
+                      />
+                      {option}
+                    </label>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <SearchSkills
+              selectedQualification={selectedQualification}
+              initialSelectedSkills={selectedSkills}
+              getSelectedSkills={(skill) => setSelectedSkills(skill)}
+            />
             <div className={style["search_qualification_container"]}>
-              <p>Search by qualifications</p>
+              <p>Search by worksite</p>
               <div>
-                {qualificationsList.map((option) => (
+                {workSiteList.map((option) => (
                   <label key={option}>
                     <input
                       type="checkbox"
-                      checked={selectedQualification.includes(option)}
-                      onChange={() => handleOptionQualificationChange(option)}
+                      checked={selectedWorkSite.includes(option)}
+                      onChange={() => handleOptionWorkSiteChange(option)}
                     />
                     {option}
                   </label>
                 ))}
-              </div>
+              </div>{" "}
             </div>
-          </div>
-          <SearchSkills
-            selectedQualification={selectedQualification}
-            initialSelectedSkills={selectedSkills}
-            getSelectedSkills={(skill) => setSelectedSkills(skill)}
-          />
-          <div className={style["search_qualification_container"]}>
-            <p>Search by worksite</p>
-            <div>
-              {workSiteList.map((option) => (
-                <label key={option}>
-                  <input
-                    type="checkbox"
-                    checked={selectedWorkSite.includes(option)}
-                    onChange={() => handleOptionWorkSiteChange(option)}
+            {(selectedWorkSite.includes("On Site") ||
+              selectedWorkSite.includes("Hybrid")) && (
+              <div className={style["search_location_container"]}>
+                <p>Search by location</p>
+                <div>
+                  <CSCSelector
+                    initialCountry={country}
+                    initialState={state}
+                    initialCity={city}
+                    getCountry={(country) => setCountry(country)}
+                    getState={(state) => setState(state)}
+                    getCity={(city) => setCity(city)}
                   />
-                  {option}
-                </label>
-              ))}
-            </div>{" "}
-          </div>
-          {(selectedWorkSite.includes("On Site") ||
-            selectedWorkSite.includes("Hybrid")) && (
-            <div className={style["search_location_container"]}>
-              <p>Search by location</p>
-              <div>
-                <CSCSelector
-                  initialCountry={country}
-                  initialState={state}
-                  initialCity={city}
-                  getCountry={(country) => setCountry(country)}
-                  getState={(state) => setState(state)}
-                  getCity={(city) => setCity(city)}
-                />
+                </div>
               </div>
+            )}
+            <div className={style["button_container"]}>
+              <button onClick={handleSearch}>Search</button>
+              <button onClick={handleClearAll}>Clear All</button>
             </div>
-          )}
-          <div className={style["button_container"]}>
-            <button onClick={handleSearch}>Search</button>
-            <button onClick={handleClearAll}>Clear All</button>
           </div>
-        </div>
-        <ul ref={contractorListRef}>
-          {contractors.length > 0 ? (
-            <>
-              <div className={style["message"]}>
-                {contractors.length === 1
-                  ? "1 match"
-                  : `${contractors.length} matches`}{" "}
-                for your project!
-              </div>
-              {contractors.map((contractor) => (
-                <div
-                  className={style["contractor_container"]}
-                  key={contractor?.id}
-                  onClick={() => {
-                    navigate(`/contractor/${contractor?.id}`);
-                  }}
-                >
-                  <div className={style["result_container"]}>
-                    <div>
-                      {contractor.percentMatching ? (
-                        <p>{contractor.percentMatching}%</p>
+        )}
+        {!selectedContractor && (
+          <ul ref={contractorListRef}>
+            {contractors.length > 0 ? (
+              <>
+                <div className={style["message"]}>
+                  {contractors.length === 1
+                    ? "1 match"
+                    : `${contractors.length} matches`}{" "}
+                  for your project!
+                </div>
+                {contractors.map((contractor) => (
+                  <div
+                    className={style["contractor_container"]}
+                    key={contractor?.id}
+                    onClick={() => {
+                      setScrollPosition(window.scrollY);
+                      setSelectedContractor(contractor);
+                    }}
+                  >
+                    <div className={style["result_container"]}>
+                      <div>
+                        {contractor.percentMatching ? (
+                          <p>{contractor.percentMatching}%</p>
+                        ) : (
+                          <p></p>
+                        )}
+                      </div>
+                      {contractor?.profileImg ? (
+                        <img src={contractor?.profileImg} alt="Profile" />
                       ) : (
-                        <p></p>
+                        <img src={avatarURL} alt="Avatar" />
                       )}
                     </div>
-                    {contractor?.profileImg ? (
-                      <img src={contractor?.profileImg} alt="Profile" />
-                    ) : (
-                      <img src={avatarURL} alt="Avatar" />
-                    )}
-                  </div>
-                  <div className={style["result_info"]}>
-                    <div>
-                      <p>
-                        {contractor?.firstName}&nbsp;{contractor?.lastName}
-                        &nbsp;
-                      </p>
-                      <div
-                        className={style["contractor_qualification_worksite"]}
-                      >
-                        <div>{contractor?.qualification}</div>
-                        <div>{contractor?.workSite}</div>
+                    <div className={style["result_info"]}>
+                      <div>
+                        <p>
+                          {contractor?.firstName}&nbsp;{contractor?.lastName}
+                          &nbsp;
+                        </p>
+                        <div
+                          className={style["contractor_qualification_worksite"]}
+                        >
+                          <div>{contractor?.qualification}</div>
+                          <div>{contractor?.workSite}</div>
+                        </div>
+                      </div>
+                      <div className={style["contractor_summary"]}>
+                        {contractor.summary}
+                      </div>
+                      <div>
+                        {contractor?.skills && (
+                          <div className={style["contractor_skills"]}>
+                            {contractor?.skills.map((resultSkill, index) => {
+                              return (
+                                <span key={index} className={style["badge"]}>
+                                  {resultSkill.skill}
+                                </span>
+                              );
+                            })}
+                          </div>
+                        )}
+                      </div>
+                      <div>
+                        <img
+                          src={favContractor}
+                          className={style["favorite_contractor"]}
+                          alt="Favourite Contractor"
+                        />
                       </div>
                     </div>
-                    <div className={style["contractor_summary"]}>
-                      {contractor.summary}
-                    </div>
-                    <div>
-                      {contractor?.skills && (
-                        <div className={style["contractor_skills"]}>
-                          {contractor?.skills.map((resultSkill, index) => {
-                            return (
-                              <span key={index} className={style["badge"]}>
-                                {resultSkill.skill}
-                              </span>
-                            );
-                          })}
-                        </div>
-                      )}
-                    </div>
-                    <div>
-                      <img
-                        src={favContractor}
-                        className={style["favorite_contractor"]}
-                        alt="Favourite Contractor"
-                      />
-                    </div>
                   </div>
-                </div>
-              ))}
-            </>
-          ) : (
-            <div className={style["message"]}>
-              No contractors found for your search criteria.
-            </div>
-          )}
-        </ul>
+                ))}
+              </>
+            ) : (
+              <div className={style["message"]}>
+                No contractors found for your search criteria.
+              </div>
+            )}
+          </ul>
+        )}
+        {selectedContractor && (
+          <ContractorProfile
+            data={selectedContractor}
+            onClose={() => setSelectedContractor(null)}
+          />
+        )}
       </main>
     </div>
   );
